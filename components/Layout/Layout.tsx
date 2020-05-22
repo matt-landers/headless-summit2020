e796f283f02fc0cfa3c759fcd1c05b1cc107f927
Matt Landers <matt@platypi.io>
2020-05-21 21:01:57 -0500

    fixed fonts on iphone

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,5 +1,5 @@
 import { Navbar } from "../Navbar/Navbar";
-
+import Head from "next/head";
 import styles from "./Layout.module.scss";
 import { Footer } from "../Footer/Footer";
 
@@ -12,6 +12,12 @@ export const Layout: React.FunctionComponent<LayoutProps> = ({
   children,
 }) => (
   <div className={styles.layout}>
+    <Head>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap"
+        rel="stylesheet"
+      />
+    </Head>
     <Navbar />
     <div className={`${styles.main} ${className}`}>{children}</div>
     <div className={styles.backgroundContainer}>
